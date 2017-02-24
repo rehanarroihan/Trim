a93025f81cd3ff6c51bc89d8541267f603f6aead
rehanarroihan <third2014project@gmail.com>
2017-02-25 01:50:17 +0700

    Navigation drawer function & add fragments

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl50413212734/trim/HelpActivity.java
@@ -0,0 +1,14 @@
+package id.sch.smktelkom_mlg.project2.xirpl50413212734.trim;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        setTitle("Help");
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl50413212734/trim/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl50413212734/trim/MainActivity.java
@@ -1,9 +1,13 @@
 package id.sch.smktelkom_mlg.project2.xirpl50413212734.trim;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +16,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    TextView tvUsernameDrawer, tvEmailDrawer;
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDB;
+    private DatabaseReference mDBuser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +56,50 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        changePage(R.id.nav_home);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        //Brain code start here
+        View hView = navigationView.getHeaderView(0); //Mengambil view dari drawer
+        tvUsernameDrawer = (TextView) hView.findViewById(R.id.textViewUsernameDrawer);
+        tvEmailDrawer = (TextView) hView.findViewById(R.id.textViewEmailDrawer);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDB = FirebaseDatabase.getInstance();
+        mDBuser = mDB.getReference().child("User");
+
+        //------------START MENGAMBIL DATA USERNAME DARI ACCOUNT----------//
+        DatabaseReference userName = mDBuser.child(mAuth.getCurrentUser().getUid()).child("username");
+        userName.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String username = dataSnapshot.getValue(String.class);
+                tvUsernameDrawer.setText(username);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        //------------END MENGAMBIL DATA USERNAME DARI ACCOUNT----------//
+
+        //------------START MENGAMBIL DATA EMAIL DARI ACCOUNT----------//
+        DatabaseReference eMail = mDBuser.child(mAuth.getCurrentUser().getUid()).child("email");
+        eMail.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String email = dataSnapshot.getValue(String.class);
+                tvEmailDrawer.setText(email);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        //------------END MENGAMBIL DATA EMAIL DARI ACCOUNT----------//
     }
 
     @Override
@@ -77,25 +137,50 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
+        changePage(id);
+        return true;
+    }
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+    private void changePage(int id) {
+        Fragment fragment = null;
+        if (id == R.id.nav_home) {
+            fragment = new HomeFragment();
+            setTitle("Home");
+        } else if (id == R.id.nav_todolist) {
+            fragment = new TodoFragment();
+            setTitle("ToDoList");
+        } else if (id == R.id.nav_notes) {
+            fragment = new NotesFragment();
+            setTitle("Notes");
+        } else if (id == R.id.nav_money) {
+            fragment = new MoneyFragment();
+            setTitle("Money Management");
+        } else if (id == R.id.nav_help) {
+            Intent h = new Intent(MainActivity.this, HelpActivity.class);
+            startActivity(h);
+        } else if (id == R.id.nav_about) {
+            Intent a = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(a);
+        } else if (id == R.id.nav_logout) {
+            mAuth.signOut();
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            new AlertDialog.Builder(this)
+                    .setTitle(getResources().getString(R.string.failed))
+                    .setMessage(getResources().getString(R.string.failed1_message))
+                    .setCancelable(true)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent b = new Intent(MainActivity.this, SplashActivity.class);
+                            startActivity(b);
+                            finish();
+                        }
+                    }).show();
         }
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commitNow();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
